83da4ea5cad4c77c327415d7f7860b9581e51013
griffin-stewie <panterathefamilyguy@gmail.com>
2021-10-02 08:42:58 +0900

    Revert "Merge pull request #2 from griffin-stewie/fix/Response"
    This reverts commit 7507308aee2e92bd5796bcb307e976facaad3807, reversing
    changes made to 7a1d1837bfe4dac693aec058e2847624fdaddd33.

--- Sources/Alfred/Response.swift
+++ Sources/Alfred/Response.swift
@@ -1,39 +1,28 @@
 import Foundation
 
-public struct Response<Element> where Element: ItemRepresentable {
-    var items: [Element]
+public struct Response: Encodable {
+    var items: [Item]
 
-    public init(items: [Element] = []) {
+    public init(items: [Item] = []) {
         self.items = items
     }
 
-    public mutating func append(item: Element) {
-        items.append(item)
+    public mutating func append(item: ItemRepresentable) {
+        items.append(item.item)
     }
 
-    public mutating func append(contentsOf items: [Element]) {
-        self.items.append(contentsOf: items)
+    public mutating func append(contentsOf items: [ItemRepresentable]) {
+        self.items.append(contentsOf: items.map { $0.item })
     }
 
     public func output() throws -> String {
-        let json = try Self.encoder().encode(self)
+        let json = try Self.encoder.encode(self)
         return String(data: json, encoding: .utf8)!
     }
 
-    static func encoder() -> JSONEncoder {
+    static var encoder: JSONEncoder = {
         let encoder = JSONEncoder()
         encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
         return encoder
-    }
-}
-
-extension Response: Encodable {
-    private enum CodingKeys: String, CodingKey {
-        case items
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(items.map(\.item), forKey: .items)
-    }
+    }()
 }
