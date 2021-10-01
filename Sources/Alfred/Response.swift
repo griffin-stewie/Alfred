47c86b5a2dd34bf022147c7ed68784eb65c2279e
griffin-stewie <panterathefamilyguy@gmail.com>
2021-10-02 08:34:42 +0900

    fix item handlings
    Now, Response type handles ItemRepresentable directly, so the user does not need to touch Item type.

--- Sources/Alfred/Response.swift
+++ Sources/Alfred/Response.swift
@@ -1,28 +1,39 @@
 import Foundation
 
-public struct Response: Encodable {
-    var items: [Item]
+public struct Response<Element> where Element: ItemRepresentable {
+    var items: [Element]
 
-    public init(items: [Item] = []) {
+    public init(items: [Element] = []) {
         self.items = items
     }
 
-    public mutating func append(item: ItemRepresentable) {
-        items.append(item.item)
+    public mutating func append(item: Element) {
+        items.append(item)
     }
 
-    public mutating func append(contentsOf items: [ItemRepresentable]) {
-        self.items.append(contentsOf: items.map { $0.item })
+    public mutating func append(contentsOf items: [Element]) {
+        self.items.append(contentsOf: items)
     }
 
     public func output() throws -> String {
-        let json = try Self.encoder.encode(self)
+        let json = try Self.encoder().encode(self)
         return String(data: json, encoding: .utf8)!
     }
 
-    static var encoder: JSONEncoder = {
+    static func encoder() -> JSONEncoder {
         let encoder = JSONEncoder()
         encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
         return encoder
-    }()
+    }
+}
+
+extension Response: Encodable {
+    private enum CodingKeys: String, CodingKey {
+        case items
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(items.map(\.item), forKey: .items)
+    }
 }
